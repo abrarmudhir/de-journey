ef925697b29789195c4461c369cc0a471cffa026
Abrar Mudhir <abrar@sehha.io>
2026-09-16 22:27:24 +0100

    SEHHACA-220: Data Pipeline Automation in Snowflake

--- assets/sql/snowflake/data-pipeline/stream.sql
+++ assets/sql/snowflake/data-pipeline/stream.sql
@@ -4,3 +4,11 @@ ON TABLE logistics.shipments;
 CREATE STREAM delivery_events_stream
 ON TABLE logistics.delivery_events
 APPEND_ONLY = TRUE;
+
+SELECT
+    product,
+    quantity,
+    METADATA$ACTION,
+    METADATA$ISUPDATE,
+    METADATA$ROW_ID
+FROM shipments_stream;

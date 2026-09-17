cad7a3bd151cce5ea3116b57c9fcec4aef985735
Abrar Mudhir <abrar@sehha.io>
2026-09-17 21:42:03 +0100

    SEHHACA-220: Data Pipeline Automation in Snowflake

--- assets/sql/snowflake/data-pipeline/task.sql
+++ assets/sql/snowflake/data-pipeline/task.sql
@@ -7,3 +7,9 @@ AS
     SELECT shipment_id, region, carrier, delivery_days
     FROM logistics.staging_shipments_stream
     WHERE METADATA$ACTION = 'INSERT';
+
+CREATE TASK logistics.refresh_dashboard
+    WAREHOUSE = harbr_wh
+    SCHEDULE = 'USING CRON 0 6 * * * UTC'
+AS
+CALL logistics.refresh_ops_dashboard();
